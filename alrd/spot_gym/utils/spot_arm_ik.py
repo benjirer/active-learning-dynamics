e58e225c25d0eb917a1e9e9abadd7e185b3bb1e9
benjirer <benjackhoffman@gmail.com>
2024-07-11 12:52:24 +0200

    ik updates

--- alrd/spot_gym/utils/spot_arm_ik.py
+++ alrd/spot_gym/utils/spot_arm_ik.py
@@ -5,15 +5,14 @@ from alrd.spot_gym.utils.spot_arm_fk import SpotArmFK
 
 class SpotArmIK:
     def __init__(self):
-        spot_arm_fk = SpotArmFK()
-        self.forward_kinematics = spot_arm_fk.get_T_fk()
+        self.spot_arm_fk = SpotArmFK()
 
         # arm assets
-        self.joint_q: np.array = None
-        self.joint_qd: np.array = None
-        self.current_ee_pose: np.array = None
+        self.joint_q: np.array = np.zeros(6)
+        self.current_ee_pose: np.array = np.zeros(3)
+        self.ee_pose_b_target: np.array = np.zeros(3)
 
-        self.jacobian: np.array = None
+        self.jacobian: np.ndarray = np.zeros((3, 6))
 
         # configs
         self.ik_max_iter = 10
@@ -24,86 +23,31 @@ class SpotArmIK:
         ee_target: np.array,
         current_arm_joint_states: np.array,
     ):
-        # save current
-        # asset = self.scene[self.asset_name]
-
-        # if self.cfg.base_impedance_mode == "none":
-        #     # sync the base pose, no tracking at all
-        #     root_pos = asset.data.root_pos_w.clone()
-        #     root_pos[:, :2] = 0.0
-        #     rp_quat = math_utils.roll_pitch_quat(asset.data.root_quat_w)
-        #     root_quat = torch.cat([rp_quat[:, 1:4], rp_quat[:, 0:1]], dim=1)
-
-        # elif self.cfg.base_impedance_mode == "hard":
-        #     # hard tracking of base pose
-        #     root_pos = asset.data.root_pos_w.clone()
-        #     root_pos[:, :2] = 0.0
-        #     root_pos[:, 2] = self.cfg.base_height_target
-
-        #     rp_quat = math_utils.quat_from_euler_xy(
-        #         self.base_roll_target, self.base_pitch_target
-        #     )
-        #     root_quat = torch.cat([rp_quat[:, 1:4], rp_quat[:, 0:1]], dim=1)
-
-        # elif self.cfg.base_impedance_mode == "soft":
-        #     raise NotImplementedError(
-        #         "Soft base impedance mode is not implemented yet."
-        #     )
-
-        # else:
-        #     raise ValueError(
-        #         f"Unknown base impedance mode: {self.cfg.base_impedance_mode}"
-        #     )
-
-        # joint_pos = torch.cat(
-        #     [
-        #         asset.data.joint_pos[:, 3:4],
-        #         asset.data.joint_pos[:, 7:8],
-        #         asset.data.joint_pos[:, 11:12],
-        #         asset.data.joint_pos[:, 2:3],
-        #         asset.data.joint_pos[:, 6:7],
-        #         asset.data.joint_pos[:, 10:11],
-        #         asset.data.joint_pos[:, 1:2],
-        #         asset.data.joint_pos[:, 5:6],
-        #         asset.data.joint_pos[:, 9:10],
-        #         asset.data.joint_pos[:, 0:1],
-        #         asset.data.joint_pos[:, 4:5],
-        #         asset.data.joint_pos[:, 8:9],
-        #     ],
-        #     dim=1,
-        # )
 
         # get joint positions
         joint_pos = torch.from_numpy(current_arm_joint_states)
 
         # set as class assets
         self.joint_q = joint_pos.numpy()
-        self.joint_qd = np.zeros_like(self.joint_qd)
+        self.ee_pose_b_target = ee_target
 
-        # compute ghost Jacobian
+        # compute Jacobian
         max_iter = self.ik_max_iter
         for i in range(max_iter):
             curr_q_pre = torch.from_numpy(self.joint_q)
-            curr_q_pre.requires_grad_()
             curr_q = curr_q_pre.clone()
 
             # compute error
             self.compute_fk()
-            # TODO: is this a pose or just a position?
-            curr_ee_pose = [
-                wp.to_torch(self.ghost_ee_pos_wp[i]).unsqueeze(1)
-                for i in range(self.ghost_num_feet)
-            ]
+            curr_ee_pose = [torch.from_numpy(self.current_ee_pose)]
 
-            curr_ee_pose = torch.cat(curr_ee_pose, dim=1)
-            curr_ee_pose_error = self.ee_pose_b_target - curr_ee_pose
+            curr_ee_pose_error = torch.from_numpy(self.ee_pose_b_target) - curr_ee_pose
 
             # calculate jacobian using finite difference based on current q and qd
             self.jacobian[:] = 0.0
             self.compute_fd_jacobian(curr_q)
 
             # calculate q delta
-            # TODO: what is envs and device?
             dpdqj_compact = self.jacobian.view(self.num_envs, -1, self.num_joints)
             curr_ee_pose_error_compact = curr_ee_pose_error.view(self.num_envs, -1)
             grad = torch.matmul(
@@ -150,46 +94,23 @@ class SpotArmIK:
         self.ee_pose_w_ik[:, :, 0:2] += asset.data.root_pos_w[:, 0:2].unsqueeze(1)
 
     def compute_fk(self):
-        # wp.sim.eval_fk(
-        #     self.ghost, self.ghost.joint_q, self.ghost.joint_qd, None, self.ghost_state
-        # )
-        # wp.launch(
-        #     compute_ee_position,
-        #     dim=self.num_envs,
-        #     inputs=[self.ghost_state.body_q, self.ghost_num_links],
-        #     outputs=[
-        #         self.ghost_ee_pos_wp[0],
-        #         self.ghost_ee_pos_wp[1],
-        #         self.ghost_ee_pos_wp[2],
-        #         self.ghost_ee_pos_wp[3],
-        #     ],
-        #     device=self.device,
-        # )
-
-        self.current_ee_pose = self.forward_kinematics(self.joint_q)
+        self.current_ee_pose = self.spot_arm_fk.get_ee_position(self.joint_q)
 
     def compute_fd_jacobian(self, curr_q, eps=1e-4):
         # use auto differentiation
-        for i in range(3):  # hip, thigh, calf
+        for i in range(6):  # all 6 joints
             q = curr_q.clone()
 
-            q[self.ghost_ik_df_idx + i] += eps
-            self.ghost.joint_q.assign(wp.from_torch(q))
+            q[i] += eps
+            self.joint_q.assign(q.numpy())
             self.compute_fk()
-            f_plus = [
-                wp.to_torch(self.ghost_ee_pos_wp[i]).clone()
-                for i in range(self.ghost_num_feet)
-            ]
-            q[self.ghost_ik_df_idx + i] -= 2 * eps
-            self.ghost.joint_q.assign(wp.from_torch(q))
+            f_plus = [torch.from_numpy(self.current_ee_pose).clone()]
+            q[i] -= 2 * eps
+            self.joint_q.assign(q.numpy())
             self.compute_fk()
-            f_minus = [
-                wp.to_torch(self.ghost_ee_pos_wp[i]).clone()
-                for i in range(self.ghost_num_feet)
-            ]
+            f_minus = [torch.from_numpy(self.current_ee_pose).clone()]
 
-            for j in range(self.ghost_num_feet):
-                self.jacobian[:, j, :, 3 * j + i] = (f_plus[j] - f_minus[j]) / (2 * eps)
+            self.jacobian[:, i] = (f_plus - f_minus) / (2 * eps)
 
         # restore q
-        self.ghost.joint_q.assign(wp.from_torch(curr_q))
+        self.joint_q.assign(curr_q.numpy())
