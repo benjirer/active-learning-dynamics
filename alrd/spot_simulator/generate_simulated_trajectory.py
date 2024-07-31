e939112e8e0ccaec688ff5b7b00421c87f12e496
benjirer <benjackhoffman@gmail.com>
2024-07-31 11:45:34 +0200

    improved simulator and built data loaders

---
+++ alrd/spot_simulator/generate_simulated_trajectory.py
@@ -0,0 +1,120 @@
+import numpy as np
+from alrd.spot_simulator.spot_simulator import SpotSimulator
+import pickle
+from scipy.spatial.transform import Rotation as R
+from alrd.run_spot import SessionBuffer, DataBuffer, TransitionData, StateData, TimeData
+from alrd.spot_gym.model.robot_state import SpotState
+
+
+def generate_trajectory(b: np.ndarray, actions, initial_state):
+    """
+    Generates a simulated trajectory using SpotSimulator class.
+
+    Args:
+        b (np.ndarray): The transition parameter.
+        actions: The actions taken (loaded from collected data).
+        initial_state: The initial state of the robot (loaded from collected data).
+
+    Returns:
+        List[np.ndarray]: The simulated trajectory.
+    """
+
+    # Initialize the simulator
+    simulator = SpotSimulator(b)
+
+    # Initialize the trajectory
+    trajectory = [initial_state]
+
+    # Simulate the trajectory
+    for action in actions:
+        next_state = simulator.step(trajectory[-1], action)
+        trajectory.append(next_state)
+
+    return trajectory
+
+
+def load_data(file_path: str):
+    """
+    Load and parse data from a pickle file to get initial state and actions.
+
+    Args:
+        file_path (str): The path to pickle file.
+
+    Returns:
+        Tuple of numpy arrays for initial state and actions.
+    """
+    with open(file_path, "rb") as file:
+        data = pickle.load(file)
+
+    states_data = data.data_buffers[0].states
+
+    # parse initial state
+    initial_state_data = states_data[0]
+    initial_state_pre = initial_state_data.next_state
+    x, y, _, qx, qy, qz, qw = initial_state_pre.pose_of_body_in_vision
+    angle = R.from_quat([qx, qy, qz, qw]).as_euler("xyz", degrees=False)[2]
+    vx, vy, _, _, _, w = initial_state_pre.velocity_of_body_in_vision
+
+    hand_x, hand_y, hand_z, hand_qx, hand_qy, hand_qz, hand_qw = (
+        initial_state_pre.pose_of_hand_in_body
+    )
+    hand_rx, hand_ry, hand_rz = R.from_quat(
+        [hand_qx, hand_qy, hand_qz, hand_qw]
+    ).as_euler("xyz", degrees=False)
+    hand_vx, hand_vy, hand_vz, hand_vrx, hand_vry, hand_vrz = (
+        initial_state_pre.velocity_of_hand_in_body
+    )
+
+    sh0, sh1, el0, el1, wr0, wr1 = initial_state_pre.arm_joint_positions
+
+    initial_state_list = [
+        x,
+        y,
+        angle,
+        vx,
+        vy,
+        w,
+        hand_x,
+        hand_y,
+        hand_z,
+        hand_rx,
+        hand_ry,
+        hand_rz,
+        hand_vx,
+        hand_vy,
+        hand_vz,
+        hand_vrx,
+        hand_vry,
+        hand_vrz,
+        sh0,
+        sh1,
+        el0,
+        el1,
+        wr0,
+        wr1,
+    ]
+    initial_state = np.array(initial_state_list, dtype=np.float32)
+
+    # parse actions (skip first state)
+    actions = [np.array(s.action, dtype=np.float32) for s in states_data[1:]]
+
+    return initial_state, actions
+
+
+if __name__ == "__main__":
+    # Load transition parameter b
+    b = np.load(
+        "/home/bhoffman/Documents/MT_FS24/active-learning-dynamics/alrd/spot_simulator/transition_parameters/b_20240731-113113.npy"
+    )
+
+    # Actions and initial state
+    initial_state, actions = load_data(
+        "/home/bhoffman/Documents/MT_FS24/active-learning-dynamics/collected_data/test20240730-174534/session_buffer.pickle"
+    )
+
+    # Generate trajectory
+    trajectory = generate_trajectory(b, actions, initial_state)
+
+    # Save trajectory
+    with open("trajectory.pickle", "wb") as file:
+        pickle.dump(trajectory, file)
--- alrd/spot_simulator/spot_simulator.py
+++ alrd/spot_simulator/spot_simulator.py
@@ -1,44 +1,89 @@
 import numpy as np
+from alrd.spot_gym.utils.spot_arm_ik import SpotArmIK
 
 
 class SpotSimulator:
     def __init__(self, b: np.ndarray):
         """
         Args:
-            b (np.ndarray): The learned parameters for the state transition model.
+            b (np.ndarray): The transition parameter.
         """
         self.b = np.array(b, dtype=np.float32)
+        self.spot_arm_ik = SpotArmIK()
 
     def step(self, current_state: np.ndarray, action: np.ndarray) -> np.ndarray:
         """
         Predict the next state given the current state and action.
+        Use IK to calulculate joint positions from end effector positions for each state.
 
         Args:
             current_state (np.ndarray): The current state of the system.
             action (np.ndarray): The action taken.
 
+        State space:
+            base_x              - frame: vision
+            base_y              - frame: vision
+            heading             - frame: vision
+            base_vx             - frame: vision
+            base_vy             - frame: vision
+            base_vrot           - frame: vision
+            ee_x                - frame: body
+            ee_y                - frame: body
+            ee_z                - frame: body
+            ee_rx               - frame: body
+            ee_ry               - frame: body
+            ee_rz               - frame: body
+            ee_vx               - frame: body
+            ee_vy               - frame: body
+            ee_vz               - frame: body
+            ee_vrx              - frame: body
+            ee_vry              - frame: body
+            ee_vrz              - frame: body
+            arm_joint_positions - frame: none
+
+        Action space:
+            base_vx             - frame: body
+            base_vy             - frame: body
+            base_vrot           - frame: body
+            ee_vx               - frame: body
+            ee_vy               - frame: body
+            ee_vz               - frame: body
+            ee_vrx              - frame: body
+            ee_vry              - frame: body
+            ee_vrz              - frame: body
+
+
         Returns:
             np.ndarray: The predicted next state.
         """
         current_state = np.array(current_state, dtype=np.float32)
+        current_state_no_joints = current_state[:18]
         action = np.array(action, dtype=np.float32)
 
-        next_state = current_state + np.dot(action, self.b.T)
+        next_state_no_joints = current_state_no_joints + np.dot(action, self.b.T)
+
+        # Calculate joint positions from end effector positions
+        ee_x, ee_y, ee_z = next_state_no_joints[6:9]
+        ee_target = np.array([ee_x, ee_y, ee_z])
+
+        joint_positions = self.spot_arm_ik.calculate_ik(ee_target, current_state[18:])
+
+        next_state = np.concatenate((next_state_no_joints, joint_positions))
+
         return next_state
 
 
-# Example usage
 if __name__ == "__main__":
-    # Example parameter b (assuming it has been trained already)
-    b = np.random.rand(10, 3)  # Replace with the actual trained b
+    # Load transition parameter b
+    b = np.load(
+        "/home/bhoffman/Documents/MT_FS24/active-learning-dynamics/alrd/spot_simulator/transition_parameters/b_20240731-113113.npy"
+    )
 
-    # Initialize the simulator
+    # Initialize simulator
     simulator = SpotSimulator(b)
 
-    # Example current state and action
-    current_state = np.random.rand(1, 10)  # Replace with actual current state
-    action = np.random.rand(1, 3)  # Replace with actual action
-
-    # Predict the next state
+    # Predict next state
+    current_state = np.random.rand(1, 10)
+    action = np.random.rand(1, 3)
     next_state = simulator.step(current_state, action)
     print("Predicted next state:", next_state)
