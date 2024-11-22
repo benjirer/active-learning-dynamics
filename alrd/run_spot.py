18c16c490764c4fae5beb98b6ce256324bbd94bd
benjirer <benjackhoffman@gmail.com>
2024-11-22 16:51:02 +0100

    new policies (policy testing v1) and goal trajs (real_traj_0,1,2)

--- alrd/run_spot.py
+++ alrd/run_spot.py
@@ -620,6 +620,10 @@ if __name__ == "__main__":
     # shape = "heart"
     # shape = "infinity"
     # shape = "infinity_large"
+    # shape = "real_traj_0"
+    # shape = "real_traj_1"
+    # shape = "real_traj_2"
+
     shape = "ellipse"
     goal_file_name = f"/home/bhoffman/Documents/MT FS24/active-learning-dynamics/goal_traj/{shape}_goal_trajectory.pkl"
     with open(goal_file_name, "rb") as f:
@@ -628,12 +632,18 @@ if __name__ == "__main__":
     """============== EXPERIMENT CONFIGS =============="""
     """===== SIM-MODEL ====="""
     sim_model_run_configs = {
-        # shape goal policy testing v1
+        # shape goal policy testing v0_pre
         # "6e2362b6": (1000, 2),
         # "y7wdkxaz": (5000, 2),
         # "n6ljjfx7": (13000, 2),
-        # shape goal policy testing v2 (faster)
-        "fq3hmij4": (5000, 1),
+        #
+        # shape goal policy testing v0 (faster)
+        # "fq3hmij4": (5000, 1),
+        #
+        # shape goal policy testing v1 (faster)
+        "kbj60y0e": (1000, 2),
+        "8i870q50": (2000, 2),
+        "ranz2azn": (5000, 2),
     }
 
     exp_config_1 = {
@@ -643,14 +653,21 @@ if __name__ == "__main__":
 
     """===== BNN-SIM-FSVGD ====="""
     bnn_sim_fsvgd_run_configs = {
-        # shape goal policy testing v0
+        # shape goal policy testing vPre
         # "21xg6mx1": (13000, 2),
-        # shape goal policy testing v1
+        #
+        # shape goal policy testing v0_pre
         # "d8qdgakj": (1000, 2),
         # "9oitc1lv": (5000, 2),
         # "1sp7yxxx": (13000, 2),
-        # shape goal policy testing v2 (faster)
-        "rgjukl9h": (5000, 1),
+        #
+        # shape goal policy testing v0 (faster)
+        # "rgjukl9h": (5000, 1),
+        #
+        # shape goal policy testing v1 (faster)
+        "7uyh9icp": (1000, 2),
+        "oz9hsvo9": (2000, 2),
+        "3qg74im1": (5000, 2),
     }
 
     exp_config_2 = {
@@ -661,12 +678,18 @@ if __name__ == "__main__":
     """===== BNN-FSVGD ====="""
 
     bnn_fsvgd_run_configs = {
-        # shape goal policy testing v1
+        # shape goal policy testing v0_pre
         # "c0vu1i05": (1000, 2),
         # "pgza5rk5": (5000, 2),
         # "sr1kau8u": (13000, 2),
-        # shape goal policy testing v2 (faster)
-        "o7e1vm9i": (5000, 1),
+        #
+        # shape goal policy testing v0 (faster)
+        # "o7e1vm9i": (5000, 1),
+        #
+        # shape goal policy testing v1 (faster)
+        "6xplabpt": (1000, 2),
+        "se9q4nvc": (2000, 2),
+        "bw6l5a2x": (5000, 2),
     }
 
     exp_config_3 = {
@@ -675,18 +698,18 @@ if __name__ == "__main__":
     }
 
     """============== SETTINGS =============="""
-    download_mode = False  # use to download policy from wandb
+    download_mode = True  # use to download policy from wandb
     num_episodes = 1
     # num_steps = 149
     num_steps = len(goal_trajectory) - 1
     cmd_freq = 15
     collect_data = True
-    project_name = "shape_tracing_v0"
+    project_name = "shape_tracing_v1"
     data_tag = project_name
 
     """============== SET ACTIVE CONFIG =============="""
     active_config_id = 2
-    active_run_id = 0
+    active_run_id = 2
     active_goal_id = shape
     num_frame_stack = 2
     action_scale = 1.0
