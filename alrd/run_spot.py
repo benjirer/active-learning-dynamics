16bb4d1b5375a993c3516513e3019479a97ea2b0
benjirer <benjackhoffman@gmail.com>
2024-11-26 12:09:20 +0100

    new policies

--- alrd/run_spot.py
+++ alrd/run_spot.py
@@ -674,7 +674,7 @@ if __name__ == "__main__":
     """===== BNN-SIM-FSVGD ====="""
     bnn_sim_fsvgd_run_configs = {
         # shape goal policy testing vPre
-        "21xg6mx1": (13000, 2),
+        # "21xg6mx1": (13000, 2),
         #
         # shape goal policy testing v0_pre
         # "d8qdgakj": (1000, 2),
@@ -695,9 +695,13 @@ if __name__ == "__main__":
         # "90n6i93n": (10000, 1),
         #
         # shape_tracing_v5
-        "rpp9kguo": (1000, 2),
-        "qio99wgo": (4000, 2),
-        "dmpc43qv": (13000, 2),
+        # "rpp9kguo": (1000, 2),
+        # "qio99wgo": (4000, 2),
+        # "dmpc43qv": (13000, 2),
+        #
+        # shape_tracing_v7
+        "u2vx0t1a": (1000, 2),
+        "uwi0v5mb": (4000, 2),
     }
 
     exp_config_2 = {
@@ -754,8 +758,8 @@ if __name__ == "__main__":
     data_tag = project_name
 
     """============== SET ACTIVE CONFIG =============="""
-    active_config_id = 2
-    active_run_id = 2
+    active_config_id = 1
+    active_run_id = 1
     active_goal_id = shape
     num_frame_stack = 2
     action_scale = 1.0
