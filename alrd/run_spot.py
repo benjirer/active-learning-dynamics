8d4515c93a7ce3594ff469abd8403f19a8d71324
benjirer <benjackhoffman@gmail.com>
2024-11-26 15:44:51 +0100

    new slalom data

--- alrd/run_spot.py
+++ alrd/run_spot.py
@@ -634,7 +634,7 @@ if __name__ == "__main__":
     # shape = "real_traj_1"
     # shape = "real_traj_2"
 
-    shape = "ellipse_large"
+    shape = "slalom"
     goal_file_name = f"/home/bhoffman/Documents/MT FS24/active-learning-dynamics/goal_traj/{shape}_goal_trajectory.pkl"
     with open(goal_file_name, "rb") as f:
         goal_trajectory = pickle.load(f)
@@ -776,8 +776,8 @@ if __name__ == "__main__":
     data_tag = project_name
 
     """============== SET ACTIVE CONFIG =============="""
-    active_config_id = 2
-    active_run_id = 5
+    active_config_id = 1
+    active_run_id = 4
     active_goal_id = shape
     num_frame_stack = 2
     action_scale = 1.0
